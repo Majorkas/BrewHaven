e3d2b3df2bc1c4d9bc44a7adbc3a929e78542be1
Majorkas <aaronrichards46@gmail.com>
2025-02-26 14:55:22 +0000

    moving project to git

---
+++ display.js
@@ -0,0 +1,7 @@
+function setAction(form) {
+    var fname = document.getElementById ("myform").elements.namedItem("name").value
+    ;
+
+
+    alert("Thanks  " + fname + " You have been subscribed to the newsletter") ;
+}
